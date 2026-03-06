1cd14001dabf2a5739077279a1f6e8617f314eaf
Nars <narsuluumamadinova@gmail.com>
2026-03-06 07:33:28 -0800

    Add files via upload

---
+++ JS/hangman.js
@@ -0,0 +1,11 @@
+/**
+ * Project Name: Hangman Game 
+ * Project Description: This project is a console-based Hangman game built using pure JavaScript.  
+ * The program randomly selects a word and the player must guess it one letter at a time before running out of attempts. 
+ * If the guessed letter is correct, it is revealed in the word. If it is incorrect, the remaining attempts decrease.  
+ * The game ends when the player either guesses the full word or reaches the maximum number of wrong guesses.
+
+ * Student Name: Narsuluu Mamadinova 
+ * 
+ * 
+ */
